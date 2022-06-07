32dea9f3fe6ffcc2b835523cd7bd7acd248041b3
Shruti2103 <shurtidevani21@gmail.com>
2022-06-07 17:37:28 +0100

    wee-18-dummy-restapi-pro2

---
+++ src/main/java/com/restapiexample/dummy/constants/EndPoints.java
@@ -0,0 +1,27 @@
+package com.restapiexample.dummy.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of RESTFUL api
+     */
+    public static final String GET_ALL_EMPLOYEE = "/employee";
+    public static final String CREATE_SINGLE_EMPLOYEE = "/employee";
+    public static final String GET_SINGLE_EMPLOYEE_BY_ID = "employee/{employeeID}";
+    public static final String UPDATE_EMPLOYEE_BY_ID = "employee/{employeeID}";
+    public static final String DELETE_EMPLOYEE_BY_ID = "employee/{employeeID}";
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/restapiexample/dummy/constants/Path.java
@@ -0,0 +1,16 @@
+package com.restapiexample.dummy.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for RESTAPI api
+     */
+    public static final String USERS = "";
+
+
+
+
+
+}
